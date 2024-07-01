9d182d45dd4bd4e9754c8d582e0b18d11819cd2c
Vanshaj16 <162701523+Vanshaj16@users.noreply.github.com>
2024-07-01 20:40:06 +0530

    Update MyProject.sol

--- contracts/MyProject.sol
+++ contracts/MyProject.sol
@@ -1,67 +1,22 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 //import "hardhat/console.sol";
 
 contract MyProject {
     address payable public owner;
-    uint256 public balance;
+    uint public balance;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
-    event InterestAdded(uint256 interestAmount);
-    event ServiceChargeDeducted(uint256 serviceChargeAmount);
+    event InterestAdded(uint interestAmount);
+    event Deposit(uint amount);
+    event ServiceChargeDeducted(uint serviceChargeAmount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
-
-    function getBalance() public view returns(uint256) {
-        return balance;
-    }
-
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
-    }
-
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-        payable(msg.sender).transfer(_withdrawAmount);
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
-    }
-
-    function addInterest(uint256 interestRate) public {
+    
+    function addInterest(uint interestRate) public {
         require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
         // Calculate interest based on the interest rate
@@ -77,7 +32,26 @@ contract MyProject {
         emit InterestAdded(interestAmount);
     }
 
-    function deductServiceCharge(uint256 serviceCharge) public {
+    function deposit(uint _amount) public payable {
+        uint _previousBalance = balance;
+
+        // make sure this is the owner
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // perform transaction
+        balance += _amount;
+
+        // assert transaction completed successfully
+        assert(balance == _previousBalance + _amount);
+
+        // emit the event
+        emit Deposit(_amount);
+    }
+
+     // custom error
+    error InsufficientBalance(uint balance, uint256 withdrawAmount);
+
+    function deductServiceCharge(uint serviceCharge) public {
         require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
         // Make sure there are sufficient funds for the service charge
@@ -107,5 +81,8 @@ contract MyProject {
     receive() external payable {
         balance += msg.value;
         emit Deposit(msg.value);
+    }
+    function getBalance() public view returns(uint) {
+        return balance;
     }
 }
